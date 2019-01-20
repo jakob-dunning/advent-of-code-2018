acdbcd62995bdf4c8851f1c5b9f5b48a3afb7234
Jakob Dunning <jakob_dunning@duckdesigns.de>
2019-01-20 16:21:05 +0100

    Solved day5

--- src/Day5/Day5.php
+++ src/Day5/Day5.php
@@ -12,6 +12,52 @@ class Day5 extends Day
 
     public function run()
     {
+        $output = $this->removeAdjacentMatchingLetters($this->input);
+        echo 'Part 1, output length: ' . strlen($output) . "\n\n";
+        
+        foreach (range('a', 'z') as $letter) {
+            $input = str_ireplace($letter, '', $this->input);
+            $currentStringLength = strlen($this->removeAdjacentMatchingLetters($input));
+            
+            if (isset($shortestStringLength) === false || $currentStringLength < $shortestStringLength) {
+                $shortestStringLength = $currentStringLength;
+            }
+        }
+        
+        echo 'Part 2, output length: ' . $shortestStringLength . "\n";
+    }
+
+    private function removeAdjacentMatchingLetters(string $input)
+    {
+        do {
+            $inputbeforeReplacement = $input;
+            
+            foreach (range('a', 'z') as $letter) {
+                $letterUpperCase = strtoupper($letter);
+                $input = str_replace(
+                    [
+                        $letter . $letterUpperCase,
+                        $letterUpperCase . $letter
+                    ],
+                    '',
+                    $input);
+            }
+        } while ($inputbeforeReplacement !== $input);
+        
+        return $input;
+    }
+
+    private function caseIsDifferent(string $match): bool
+    {
+        if (ctype_upper($match[0]) && ctype_lower($match[1])) {
+            return true;
+        }
+        
+        if (ctype_upper($match[1]) && ctype_lower($match[0])) {
+            return true;
+        }
+        
+        return false;
     }
 }
 
